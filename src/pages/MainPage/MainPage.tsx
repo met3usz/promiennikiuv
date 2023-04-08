b1d756397b51bac79e9eee2c0f91b7921bf563b7
Mateusz <m.wasik1303@gmail.com>
2023-04-08 13:43:43 +0200

    Added Hero section
    The Hero section includes components such as:
    - header
    - text
    - button
    - product images

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,5 +1,30 @@
+import HeroImg from '../../img/hero-img.png';
+import {
+  HeroButton,
+  HeroImage,
+  HeroLeft,
+  HeroText,
+  HeroTitle,
+  HeroWrapper,
+} from './styled';
+
 const MainPage = () => {
-  return <h1>I'm a Main Page!</h1>;
+  return (
+    <HeroWrapper>
+      <HeroLeft>
+        <HeroTitle>Promienniki IRT i Myjnie DRESTER</HeroTitle>
+        <HeroText>
+          Firma Hedson Technologies AB jest producentem najwyższej klasy
+          promienników podczerwieni przeznaczonych do szybkiego suszenia
+          powierzchni lakierowanych z przeznaczeniem miedzy innymi do lakierni
+          samochodowych.
+        </HeroText>
+        <HeroButton>Skontaktuj się z nami!</HeroButton>
+      </HeroLeft>
+
+      <HeroImage src={HeroImg} />
+    </HeroWrapper>
+  );
 };
 
 export default MainPage;
---
+++ src/pages/MainPage/styled.ts
@@ -0,0 +1,50 @@
+import styled from 'styled-components';
+
+export const HeroWrapper = styled.div`
+  margin: 0;
+  padding: 90px 60px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+`;
+
+export const HeroLeft = styled.div`
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 24px;
+`;
+
+export const HeroTitle = styled.h1`
+  margin: 0;
+  padding: 0;
+  font-size: 52px;
+`;
+
+export const HeroText = styled.p`
+  margin: 0;
+  padding: 0;
+  font-size: 18px;
+  color: ${({ theme }) => theme.color.secondaryText};
+`;
+
+export const HeroImage = styled.img`
+  width: 150%;
+  max-width: 1000px;
+  height: auto;
+`;
+
+export const HeroButton = styled.button`
+  margin: 0;
+  padding: 15px;
+  color: ${({ theme }) => theme.color.buttonText};
+  outline: none;
+  border: none;
+  border-radius: ${({ theme }) => theme.borderRadius};
+  background-color: ${({ theme }) => theme.color.primatyDrester};
+`;
