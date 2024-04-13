285eeed458309ac9bab8e1ad980f30c842a6c785
Mateusz Wąsik <m.wasik1303@gmail.com>
2024-04-13 11:42:53 +0200

    Update
    - updated npm packages & emailjs

--- src/emailJScredentials.ts
+++ src/emailJScredentials.ts
@@ -1,3 +1,3 @@
-export const SERVICE_ID = 'service_zxnetq2';
+export const SERVICE_ID = 'service_29i4wgl';
 export const TEMPLATE_ID = 'template_u73ruic';
 export const USER_ID = '1Hz09AIsj_stKXoOG';
